aa188ffd5ec7b0465fe3c643448f6107f5f38388
yhetman <juliagetman5@gmail.com>
2021-10-18 23:23:40 +0300

    |check|

--- src/main.c
+++ src/main.c
@@ -50,15 +50,15 @@ init_t_aes(t_aes *aes, uint8_t *key, uint8_t *input)
 }
 
 
-static void
-print_uint8_t(uint8_t *str)
-{
-	uint8_t i;
+// static void
+// print_uint8_t(uint8_t *str)
+// {
+// 	uint8_t i;
 
-	for (i = 0; i < 4; i++)
-		printf("%02x %02x %02x %02x " , str[4 * i + 0], str[4 * i + 1], str[4 * i + 2], str[4 * i + 3]);
-	printf("\n");
-}
+// 	for (i = 0; i < 4; i++)
+// 		printf("%02x %02x %02x %02x " , str[4 * i + 0], str[4 * i + 1], str[4 * i + 2], str[4 * i + 3]);
+// 	printf("\n");
+// }
 
 
 int
@@ -111,7 +111,7 @@ main(int argc, char **argv)
 		cipher(&aes);
 
 		// printf("Ciphered text:\n");
-		print_uint8_t(aes.cipher_text);
+		// print_uint8_t(aes.cipher_text);
 
 		// decipher(&aes);
 
